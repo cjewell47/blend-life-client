135aba0c72fe04450becbdca6fc05c038db206fe
cjewell47 <charles.jewell47@gmail.com>
2017-06-02 11:54:34 +0100

    reg log controllers set up

---
+++ src/js/controllers/login.controller.js
@@ -0,0 +1,19 @@
+angular
+  .module('BlendLife')
+  .controller('LoginCtrl', LoginCtrl);
+
+
+LoginCtrl.$inject = ['User'];
+function LoginCtrl(User) {
+  const vm = this;
+
+  vm.login = () => {
+    User.login(vm.user)
+    .$promise
+    .then(data => {
+      console.log(data);
+    }, err => {
+      console.log(err);
+    });
+  };
+}
