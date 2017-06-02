7d6df2a9c8fc84791f72f0b590ca375b33c4fb1b
cjewell47 <charles.jewell47@gmail.com>
2017-06-02 14:15:32 +0100

    routes and forms done for register and login

--- src/js/controllers/register.controller.js
+++ src/js/controllers/register.controller.js
@@ -5,15 +5,20 @@ angular
 RegisterCtrl.$inject = ['User', 'CurrentUserService'];
 function RegisterCtrl(User, CurrentUserService) {
   const vm    = this;
+  vm.emailValidate = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  vm.register =  () => {
-    User
-    .register(vm.user)
-    .$promise
-    .then(() => {
-      CurrentUserService.getUser();
-    }, err => {
-      console.log(err);
-    });
-  };
+  vm.register = register;
+
+  function register() {
+    if (vm.registerForm.$valid) {
+      User
+      .register(vm.user)
+      .$promise
+      .then(() => {
+        CurrentUserService.getUser();
+      }, err => {
+        console.log(err);
+      });
+    }
+  }
 }
