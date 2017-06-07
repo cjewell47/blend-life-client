ae1078fc3099b10bb78ba4f5de8aab610fc8b7d4
cjewell47 <charles.jewell47@gmail.com>
2017-06-07 15:38:14 +0100

    hard coded in percentages for 7 and 8 ingredients

--- src/js/controllers/recipes/recipes-new.controller.js
+++ src/js/controllers/recipes/recipes-new.controller.js
@@ -22,8 +22,18 @@ function RecipeNewCtrl (Recipe, $state, Ingredient, $window) {
       return new ColorMix.Color(arr[0], arr[1], arr[2]);
     });
 
-    const percents = new Array(colors.length).fill(100 / colors.length);
+    // const percents = new Array(colors.length).fill(100 / colors.length);
+    let percents = [];
+    if(colors.length === 6) {
+      percents = [17.5, 16.5, 16.5, 16.5, 16.5, 16.5];
+    } else if (colors.length === 7) {
+      percents = [14.8, 14.2, 14.2, 14.2, 14.2, 14.2, 14.2];
+    } else {
+      percents = new Array(colors.length).fill(100 / colors.length);
+    }
+
     console.log(percents);
+
 
     const mix = ColorMix.mix(colors, percents);
     vm.combinationColor = `${mix.red}, ${mix.green}, ${mix.blue}`;
