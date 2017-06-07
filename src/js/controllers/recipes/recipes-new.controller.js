966e405f524e2940510e230a01201c5b5d61e247
cjewell47 <charles.jewell47@gmail.com>
2017-06-07 13:13:01 +0100

    colour now changing upon ingredient selection

--- src/js/controllers/recipes/recipes-new.controller.js
+++ src/js/controllers/recipes/recipes-new.controller.js
@@ -2,21 +2,26 @@ angular
 .module('BlendLife')
 .controller('RecipeNewCtrl', RecipeNewCtrl);
 
-RecipeNewCtrl.$inject = ['Recipe', '$state', 'Ingredient', 'filterFilter'];
-function RecipeNewCtrl (Recipe, $state, Ingredient, filterFilter) {
+RecipeNewCtrl.$inject = ['Recipe', '$state', 'Ingredient'];
+function RecipeNewCtrl (Recipe, $state, Ingredient) {
   const vm = this;
 
   vm.selectedIngredients = [];
   vm.ingredients         = Ingredient.query();
   vm.create              = recipeCreate;
   vm.selectIngredient    = selectIngredient;
+  // vm.getColour            = getColour;
 
   function selectIngredient(event, ingredient) {
     vm.selectedIngredients.indexOf(ingredient) === -1 ? vm.selectedIngredients.push(ingredient) : vm.selectedIngredients.splice(vm.selectedIngredients.indexOf(ingredient), 1);
   }
 
+  // function getColour() {
+  //   const ingredientColour = vm.selectedIngredients[0].colour;
+  //   return `rgb(${ingredientColour})`;
+  // }
+
   function recipeCreate(){
-    // filter vm.selectedIngredients into a new array containing just id's from ingredients
     vm.selectedIngredient_ids = vm.selectedIngredients.map(function(a) {
       return a.id;
     });
@@ -38,27 +43,3 @@ function RecipeNewCtrl (Recipe, $state, Ingredient, filterFilter) {
     }
   }
 }
-
-// vm.select = select;
-
-// function filterIngredients() {
-//   const params = vm.recipe.ingredient_ids;
-//   vm.selectedIngredients = [];
-//   params.forEach(id => {
-//     vm.selectedIngredients.push(filterFilter(vm.ingredients, {id: id})[0].colour);
-//   });
-// }
-
-// function select($scope) {
-//   console.log('scope ingredient', $scope.ingredient);
-//   console.log(vm.selected);
-//   if(vm.selected.indexOf($scope.ingredient) === -1) {
-//     vm.selected.push($scope.ingredient);
-//   } else {
-//     for(var i = 0; i < vm.selected.length; i++) {
-//       if(vm.selected[i] === $scope.ingredient) {
-//         vm.selected.splice(i, 1);
-//       }
-//     }
-//   }
-// }
